10def4d9433bbcf9f008d63cc8def5fbe9ee80d7
DnyaneshKshatriya07 <dnyaneshkshatriya7123@gmail.com>
2024-02-12 21:25:10 +0530

    UnBoxing

---
+++ Java_Basic/UnBoxing.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.SortedMap;
+
+public class UnBoxing {
+    public static void main(String[] args) {
+
+        Integer a=7;
+        Float b=3.33F;
+        Long c= 1345234L;
+
+        int x =a;
+        float y= b;
+        long z= c;
+
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+    }
+}
